9a5714ec0a395d7c674f066b5555f88a95dee173
Alejandro <alegartiz@hotmail.com>
2022-09-06 13:30:50 -0300

    Condicional+bucle+array

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// ALGORITMO CONDICIONAL
+
 let entry1, entry2, num1, num2, resultado;
 
 dato1 = prompt("Inserte un primer número", "0");
@@ -16,6 +18,74 @@ document.write(` <h3>De los números que insertó, el mayor es ${resultado}.</h3
 alert("Su resultado se presentará al final de la página")
 
 // FIN ALGORITMO CONDICIONAL
+
+
+// ============================================================================
+
+
+// Arrays 
+
+class hardware {
+    constructor(nombre, categoria, fabricante, precio, disponibilidad) {
+        this.nombre = nombre
+        this.categoria = categoria
+        this.fabricante = fabricante
+        this.precio = precio
+        this.disponibilidad = disponibilidad
+    }
+    habilitar() {
+        let error = "libro disponible"
+        if (this.disponibilidad == false) {
+            this.disponibilidad = true
+        } else {
+            console.log(error)
+        }
+    }
+
+
+
+}
+
+const dispositivos = []
+
+let hardware1 = new hardware("OculusQuest", "VRdevice", "Meta", "300", true)
+let hardware2 = new hardware("OracleVR", "VRDevice", "Oracle", "900", false)
+let hardware3 = new hardware("OculusQuest2", "VRDevice", "Meta", "599", true)
+
+dispositivos.push(hardware1, hardware2, hardware3)
+
+
+// Para habilitar los que estan en false
+hardware2.habilitar()
+
+console.log(hardware2)
+
+// Filtro por precio
+let preciomayor899 = dispositivos.filter(hardware => hardware.precio > 899)
+
+console.log(preciomayor899)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // TYPEWRITER HECHO EN EL MODULO DE DESARROLLO WEB
