0d851288f2a8f1c0dd7f26ce152930185dd91ba0
Alejandro <alegartiz@hotmail.com>
2022-09-15 18:57:58 -0300

    patch dom y eventos

--- js/dom.js
+++ js/dom.js
@@ -3,14 +3,14 @@ const span = document.getElementById('span')
 let contador = 0
 
 btnAumentar.addEventListener('click', () => {
-    console.log('Clickeaste el boton  y guardo la info')
+    console.log('Aumentaste el número')
     contador++
     span.textContent = contador
 })
 
 const btnDisminuir = document.querySelector('.btn-danger')
 btnDisminuir.addEventListener('click', () => {
-    console.log('Clickeaste el boton  y guardo la info')
+    console.log('Disminuiste el número')
     contador--
     span.textContent = contador
 })
